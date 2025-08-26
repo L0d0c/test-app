de48094fce85036a7b519e8e1217f31ba24c7036
loic binet <loicbinet21@gmail.com>
2025-08-26 19:19:44 +0200

    fix: app image

--- linkedin-clone-docker/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counterSlice';
-import userReducer from '../features/userSlice';
-
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
